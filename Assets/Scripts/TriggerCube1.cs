563dd912048079b24ced19c03de1751d0dfbfc2b
erosies <72110242+erosies@users.noreply.github.com>
2020-11-19 22:42:54 -0500

    Script to turn on red light/creepy noise
    Added script (triggercube1.cs) to turn on a red light when a users steps on a cube that acts as a trigger for the light. Can also trigger creepy noises.

---
+++ Assets/Scripts/TriggerCube1.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerCube : MonoBehaviour {
+
+    public bool onSwitch;
+    public bool lightStatus;
+    public GameObject AmbientBuzzingNoise;
+    public GameObject ScaryNoise;
+    public GameObject CreepyRedLight;
+    public GameObject NormalRoomLight;
+   
+
+  void Start()
+	{
+	CreepyRedLight.SetActive(false);
+	NormalRoomLight.SetActive(true);
+    NatureNoise.SetActive(true);
+	ScaryNoise.SetActive(false);
+	}
+        
+  void OnTriggerEnter(Collider other)
+    {
+        onSwitch = true;
+    }
+
+  void OnTriggerExit(Collider other)
+    {
+        onSwitch = false;
+    }
+
+    void Update()
+    {
+        if(CreepyRedLight.activeSelf == true)
+        {
+            lightStatus = true;
+        }
+        else
+        {
+            lightStatus = false;
+        }
+
+        if (onSwitch == true)
+        {
+            if (lightStatus){
+				CreepyRedLight.SetActive(true);
+				NormalRoomLight.SetActive(false);
+                AmbientBuzzingNoise.SetActive(false);
+				ScaryNoise.SetActive(true);
+            } else
+			{
+   				CreepyRedLight.SetActive(true);
+				NormalRoomLight.SetActive(false);
+                AmbientBuzzingNoise.SetActive(false);
+				ScaryNoise.SetActive(true);
+    		}
+        }
+    }
+}
