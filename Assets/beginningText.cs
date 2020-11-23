3bb51506b44d5cf61bf072002ea38d653efc8ded
Deja36 <dejawilliams3936@gmail.com>
2020-11-23 02:24:47 -0500

    Create beginningText.cs

---
+++ Assets/beginningText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beginningText : MonoBehaviour
+{
+    public GameObject beginTxt;
+    void Start()
+    {
+        beginTxt.SetActive(false);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            beginTxt.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(6);
+        Destroy(beginTxt);
+        Destroy(gameObject);
+    }
+}
