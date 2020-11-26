c0df1acfac5f633c51cbdf8c2a2ac9c62535b3f0
Deja36 <dejawilliams3936@gmail.com>
2020-11-25 20:05:30 -0500

    Create ventD.cs

---
+++ Assets/Scripts/ventD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ventD : MonoBehaviour
+{
+    public GameObject enterText;
+    void Start()
+    {
+        enterText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            enterText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enterText.SetActive(false);
+        }
+    }
+}
