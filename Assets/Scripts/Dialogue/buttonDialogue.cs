2e12ac2ad847d3f04a6fe09bde5e1efbe9fb5543
Deja36 <dejawilliams3936@gmail.com>
2020-11-25 20:05:42 -0500

    Create buttonDialogue.cs

---
+++ Assets/Scripts/Dialogue/buttonDialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class buttonDialogue : MonoBehaviour
+{
+    //public KeyCode key;
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+
+    }
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            FadetoColor(button.colors.pressedColor);
+            button.onClick.Invoke();
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            FadetoColor(button.colors.normalColor);
+        }
+    }
+
+    void FadetoColor(Color color)
+    {
+        Graphic graphic = GetComponent<Graphic>();
+        graphic.CrossFadeColor(color, button.colors.fadeDuration, true, true);
+    }
+}
