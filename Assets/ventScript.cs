d06dc6467d8e24b0e87fb303eff0b62d7ba2ab44
Deja36 <dejawilliams3936@gmail.com>
2020-11-25 20:05:21 -0500

    Create ventScript.cs

---
+++ Assets/ventScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ventScript : MonoBehaviour
+{
+    public GameObject ventText;
+    void Start()
+    {
+        ventText.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            ventText.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(ventText);
+        Destroy(gameObject);
+    }
+}
