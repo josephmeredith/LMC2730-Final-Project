a401a15691811b96c15fb56def6a59e3cd79868b
Deja36 <dejawilliams3936@gmail.com>
2020-12-02 02:28:38 -0500

    Update doorOC.cs

--- Assets/Scripts/_OriginalScripts/doorOC.cs
+++ Assets/Scripts/_OriginalScripts/doorOC.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class doorOC : MonoBehaviour
 {
-    // public GameObject instructions;
-    // private void OnTriggerStay(Collider other)
-    // {
-    //     if(other.tag == "doorMayar")
-    //     {
-    //         instructions.SetActive(true);
-    //         Animator anim = other.GetComponentInChildren<Animator>();
-    //         if(Input.GetKeyDown("Use"))
-    //         anim.SetTrigger("OpClo");
-    //     }
-    // }
+    public GameObject instructions;
+    private void OnTriggerStay(Collider other)
+    {
+      if(other.tag == "doorMayar")
+      {
+          instructions.SetActive(true);
+          Animator anim = other.GetComponentInChildren<Animator>();
+          if(Input.GetKeyDown(KeyCode.E))
+                anim.SetTrigger("OpenClose");
+      }
+    }
 
-    // private void OnTriggerExit(Collider other)
-    // {
-    //     if (other.tag == "doorMayar")
-    //     {
-    //         instructions.SetActive(false);        }
-    // }
+    private void OnTriggerExit(Collider other)
+    {
+       if (other.tag == "doorMayar")
+       {
+             instructions.SetActive(false);
+       }
+    }
 }
