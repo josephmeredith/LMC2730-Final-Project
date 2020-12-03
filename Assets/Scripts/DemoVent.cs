ae4c0b7ddf1d48f141111419aee5bde912b9330a
Deja36 <dejawilliams3936@gmail.com>
2020-12-03 05:01:55 -0500

    Create DemoVent.cs

---
+++ Assets/Scripts/DemoVent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class DemoVent : MonoBehaviour
+{
+    public TextMeshProUGUI ventText;
+    public bool onVent = false;
+
+    void Start() {
+        ventText.text = "Press E to interact";
+        ventText.enabled = false;
+    }
+
+    void FixedUpdate() {
+        if(Input.GetKeyDown(KeyCode.E) && onVent) {
+            Debug.Log("Press registered");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            ventText.enabled = true;
+            onVent = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ventText.enabled = false;      
+            onVent = false;
+        }
+    }
+}
