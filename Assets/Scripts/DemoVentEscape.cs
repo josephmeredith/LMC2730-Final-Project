727b0a98a17d95229198604e654b0374c94d7d12
Deja36 <dejawilliams3936@gmail.com>
2020-12-03 05:01:50 -0500

    Create DemoVentEscape.cs

---
+++ Assets/Scripts/DemoVentEscape.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DemoVentEscape : MonoBehaviour
+{
+   public Text screenText;
+
+    void Start() {
+        screenText.text = "Press E to interact.";
+        screenText.enabled = false;
+    }
+
+    void FixedUpdate() {
+        if(Input.GetButton("Interact")) {
+            Debug.Log("Press registered");
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            screenText.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            screenText.enabled = false;      
+        }
+    }
+}
