04e9707702aef29977c305e7077871219561d390
Deja36 <dejawilliams3936@gmail.com>
2020-12-05 12:38:11 -0500

    Create blockedPath.cs

---
+++ Assets/Scripts/blockedPath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockedPath : MonoBehaviour
+{
+    public GameObject uiObject;
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+}
+
