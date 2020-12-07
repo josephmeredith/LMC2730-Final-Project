4f6381fb7e70f3faf47e1bf0d8e530665287e21a
Joseph Meredith <josephmeredith112@gmail.com>
2020-12-07 12:29:31 -0500

    Project cleaned up and built

--- Assets/Scripts/triggerScene.cs
+++ Assets/Scripts/triggerScene.cs
@@ -12,17 +12,27 @@ public class triggerScene : MonoBehaviour
         uiObject.SetActive(false);
     }
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E) && uiObject.activeSelf)
+            {
+                SceneManager.LoadScene(levelToLoad);
+                // SceneManager.LoadScene("ENDING");
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+    }
+
     // Update is called once per frame
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")  
         {
             uiObject.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                SceneManager.LoadScene(levelToLoad);
-                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
+            // if (Input.GetKeyDown(KeyCode.E))
+            // {
+            //     // SceneManager.LoadScene(levelToLoad);
+            //     SceneManager.LoadScene("ENDING");
+            //     //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            // }
         }
     }
 }
